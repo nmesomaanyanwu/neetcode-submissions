6cde037d42a382567ff32581610ddebd0aecc8d9
Nmesoma Anyanwu <150448018+nmesomaanyanwu@users.noreply.github.com>
2026-07-22 04:27:44 +0100

    Add: remove-node-from-end-of-linked-list - submission-1

---
+++ Data Structures & Algorithms/remove-node-from-end-of-linked-list/submission-1.cpp
@@ -0,0 +1,79 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        /*
+        1. Reverse the linkedlist
+        2. While in the while loop keep updating a count ill you reach n then return the node value 
+        */
+
+        ListNode* current = head;
+    
+        ListNode* prev= nullptr;
+        int count = 2;
+
+        // revese
+        while (current != nullptr){
+            ListNode* next = current->next;
+            current->next = prev;
+            prev = current;
+            current = next;
+        }
+        current = prev; // head of the reversed list
+        ListNode* s = current;
+        ListNode* t = current->next; 
+
+
+        while (current != nullptr){
+            if (n == 0){
+                return head;
+            }
+            if (1 == n){
+                ListNode dummy;
+
+                dummy.next = t;
+                ListNode* s = dummy.next;
+                ListNode* prev = nullptr;
+
+                while (s != nullptr){
+                    ListNode* next = s->next;
+                    s->next = prev;
+                    prev = s;
+                    s = next;
+
+                }
+                s = prev;
+                return s;
+
+            }
+            else if (n>= 2 && count == n){
+                s->next = t->next;
+                t->next = nullptr;
+                ListNode* prev = nullptr;
+
+                while (current != nullptr){
+                    ListNode* next = current->next;
+                    current->next = prev;
+                    prev = current;
+                    current = next;
+
+                }
+                current = prev;
+                return current;
+            }
+
+        }
+        
+        
+    }
+};
