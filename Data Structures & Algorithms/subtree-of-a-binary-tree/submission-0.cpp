9347f1b5d4cce12d62206a7ad529b11c9d11ef9d
Nmesoma Anyanwu <150448018+nmesomaanyanwu@users.noreply.github.com>
2026-07-23 22:04:21 +0100

    Add: subtree-of-a-binary-tree - submission-0

---
+++ Data Structures & Algorithms/subtree-of-a-binary-tree/submission-0.cpp
@@ -0,0 +1,70 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+        /*
+        root & subRoot:
+
+        */
+        if ((root == nullptr) && (subRoot != nullptr) ){
+            return false;
+        }
+        else if ((root != nullptr) && (subRoot == nullptr)){
+            return true;
+        }
+        else if ((root == nullptr) && (subRoot == nullptr)){
+            return true;
+        }
+        else{
+            if (isSameTree(root, subRoot)){
+                 return true;
+            }
+            
+            bool left = isSubtree(root->left , subRoot);
+            bool right = isSubtree(root->right, subRoot);
+
+            if (left || right){
+                return true;
+            }
+            else{
+                return false;
+            }
+
+        }
+        
+    }
+private:
+     bool isSameTree(TreeNode* p, TreeNode* q) {
+        if (p == nullptr && q == nullptr){
+            return true;
+        }
+        else if( (p == nullptr && q!=nullptr) || (p != nullptr && q==nullptr)){
+            return false;
+        }
+
+        else{
+            if(p->val != q->val){
+                return false;
+            }
+            
+            bool left = isSameTree(p->left, q->left);
+            bool right = isSameTree(p->right, q->right);
+
+            
+    
+            return left && right;
+        }
+        
+    }
+};
