68f1a30299a03e11fca6ccffbc5fe654cf203d28
Nmesoma Anyanwu <150448018+nmesomaanyanwu@users.noreply.github.com>
2026-07-26 22:46:36 +0100

    Add: combination-target-sum-ii - submission-0

---
+++ Data Structures & Algorithms/combination-target-sum-ii/submission-0.cpp
@@ -0,0 +1,33 @@
+class Solution {
+vector<vector<int>> res;
+vector<int> path;
+public:
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        dfs(0 , candidates, target);
+        return res;  
+        
+
+
+    }
+private:
+    void dfs(int index ,vector<int>& nums, int remaining ){
+
+        if (remaining == 0){
+            if (find(res.begin(), res.end(), path) == res.end()) {
+        res.push_back(path);
+    }
+            return; 
+        }
+        if (remaining < 0){
+            return;
+        }
+
+        for (int i = index ;i < nums.size(); i++){
+            path.push_back(nums[i]);
+            dfs(i+ 1 , nums , remaining -nums[i]);
+            path.pop_back(); 
+
+        }
+    }
+
+};
