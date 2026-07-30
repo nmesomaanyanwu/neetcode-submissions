7f442fbc103155a0bf392d75f249d41328eb5048
Nmesoma Anyanwu <150448018+nmesomaanyanwu@users.noreply.github.com>
2026-07-30 20:15:35 +0100

    Add: last-stone-weight - submission-0

---
+++ Data Structures & Algorithms/last-stone-weight/submission-0.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        /*what to do :
+        1) we use a max heap 
+        2) we take the two biggest if they are equal we pop them 
+        3) if y > x we remove x and push (y - x)
+        4) if stone == 1 or less return that stone 
+        */
+        priority_queue<int> weight;
+
+        for(int i = 0 ; i < stones.size(); i++){
+            weight.push(stones[i]);
+        } 
+
+        // loop for crushing
+        while (weight.size() != 0){
+            if (weight.size() <= 1){
+                return weight.top();
+            }
+            int a = weight.top();
+            weight.pop();
+
+            int b = weight.top();
+            weight.pop();
+
+            if (a == b){
+                continue;
+            }
+            if (a > b){
+                weight.push(a-b);
+            }  
+        
+    }
+
+    }
+};
